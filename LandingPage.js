005634d9cb13568671d6ba9b62427d98276b5c78
JMCorrado <joseph.michael.corrado@gmail.com>
2020-12-28 14:17:55 -0500

    added Dan

--- LandingPage.js
+++ LandingPage.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default () => (
   <div id="landingPage">
     <h1>John Cavaseno</h1>
-    <h3>Actor | Dancer | Performer</h3>
+    <h3>Actor | Dancer | Performer | Dan</h3>
   </div>
 )
